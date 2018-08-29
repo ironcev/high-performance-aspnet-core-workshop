0a356f17beff67e74d5da98aa3d6bc4b6c3a06a1
Dobrisa Adamec <dadamec@gmail.com>
2018-08-29 14:07:01 +0200

    feat: Get JWT token controller action

---
+++ src/GettingThingsDone.WebApi/Controllers/TokenController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using GettingThingsDone.WebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GettingThingsDone.WebApi.Controllers
+{
+    [Route("api/token")]
+    [ApiController]
+    public class TokenController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public object CustomClaimTypes { get; private set; }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult RequestToken([FromBody] TokenRequest request)
+        {
+            //for sample implementation only
+            //could be AppCode for applications that use API
+            //database check behind
+            if (request.Username == "Kulendayz" && request.Password == "Kulendayz")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.Username),
+                    new Claim(ClaimTypes.Role, "ADMIN"),
+                    new Claim("CompletedBasicTraining", ""),
+                    //new Claim(CustomClaimTypes.EmploymentCommenced,
+                    //           new DateTime(2017,12,1).ToString(),
+                    //            ClaimValueTypes.DateTime)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "gettingthingsdone.com",
+                    audience: "gettingthingsdone.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }
+    }
+
+}
---
+++ src/GettingThingsDone.WebApi/Models/TokenRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GettingThingsDone.WebApi.Models
+{
+    /// <summary>
+    /// Authentication request for getting token
+    /// </summary>
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
