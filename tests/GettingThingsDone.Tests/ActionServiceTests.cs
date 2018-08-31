22483db4d97c442408cb47722e0b6e8a0f8dbd52
Toni Petrina <tonipetrina@hotmail.com>
2018-08-31 10:03:40 +0200

    Added unit test example

---
+++ tests/GettingThingsDone.Tests/ActionServiceTests.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using GettingThingsDone.ApplicationCore.Services;
+using Moq;
+using GettingThingsDone.Contracts.Interface;
+using GettingThingsDone.Contracts.Model;
+using AutoFixture.Xunit2;
+using System.Threading.Tasks;
+
+namespace GettingThingsDone.Tests
+{
+    public class ActionServiceTests
+    {
+        [Theory, AutoData]
+        public async Task Test1(int id)
+        {
+            var actionRepository = new Mock<IAsyncRepository<Action>>();
+            var listRepository = new Mock<IRepository<ActionList>>();
+            var projectRepository = new Mock<IRepository<Project>>();
+            var sut = new ActionService(actionRepository.Object, listRepository.Object, projectRepository.Object);
+
+            var result = await sut.GetAction(id);
+
+            Assert.Equal(ServiceCallStatus.EntityNotFound, result.Status);
+        }
+    }
+}
